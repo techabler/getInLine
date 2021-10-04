141ba745323b46a26d9bbaaa63744fad8dec5db1
tabler <tabler@naver.com>
2021-10-04 23:13:28 +0900

    Initial commit

---
+++ src/test/java/com/example/tabler/getInLine/GetInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabler.getInLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetInLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
